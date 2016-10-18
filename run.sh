9f68eea092ba81195993c97b70f8911ff9e6e2a3
seveniu <prime3721@gmail.com>
2016-10-18 22:35:25 +0800

    add run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-./gradlew clean
-./gradlew assemble
-jar=$(find build/libs/ -type f -name '*.jar' | awk -F './' '{print $3}')
-echo "find jar : $jar"
+profile=$1
+echo "$profile"
+if [ ! $profile ]; then
+  echo "profile is null"
+else
+  ./gradlew clean -Pprofile=$profile
+  ./gradlew assemble -Pprofile=$profile
+  jar=$(find build/libs/ -type f -name '*.jar' | awk -F './' '{print $3}')
+  echo "find jar : $jar"
 
-java -jar build/libs/$jar
+  java -jar build/libs/$jar --spring.profiles.active=$profile
+fi
