f32c808930a308bd78de06031407349c76640ef9
seveniu <prime3721@gmail.com>
2016-10-25 00:52:52 +0800

    bug fix

--- conf/schema.sql
+++ conf/schema.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS `queue`(
+CREATE TABLE `queue` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `data` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=450 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
